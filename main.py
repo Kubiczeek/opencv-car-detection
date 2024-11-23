b77823f40547bc1a2b76eacdc27c3aadc5e1c39a
Kubiczeek <jakubakubek@gmail.com>
2024-11-23 18:53:34 +0100

    feat(args): runnable from console, now accepts arguments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
+import os
 import cv2
-import numpy as np
-
+import argparse
 
 def detect_vehicles(image_path, conf_threshold=0.3):
     # Load YOLOv8 model
@@ -55,16 +55,29 @@ def detect_vehicles(image_path, conf_threshold=0.3):
     return image
 
 
-def main():
-    # Replace with your image path
-    image_path = "Cars_image.jpg"
+def main(image_path, conf_threshold=0.3, dest_path=None):
+
+    if conf_threshold < 0 or conf_threshold > 1:
+        raise ValueError("Confidence threshold must be between 0 and 1")
+
+    if not image_path:
+        raise ValueError("Image path is required")
 
     try:
         # Process image
-        result_image = detect_vehicles(image_path)
+        result_image = detect_vehicles(image_path, conf_threshold)
+
+        if not os.path.exists(dest_path):
+            os.makedirs(dest_path)
 
         # Save result
-        output_path = "detected_vehicles.jpg"
+        file_extension = image_path.split(".")[-1]
+        image_name = image_path.split("/")[-1].split(".")[0]
+        output_path = dest_path
+        if not dest_path:
+            output_path = f"{image_path.split('.')[0]}_result.{file_extension}"
+        else:
+            output_path = f"{output_path}{image_name}_result.{file_extension}"
         cv2.imwrite(output_path, result_image)
         print(f"Detection completed. Result saved to {output_path}")
 
@@ -78,4 +91,16 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Detect vehicles in an image")
+    parser.add_argument("--image", required=True, help="Path to image (include file extension)")
+    parser.add_argument("--conf", type=float, default=0.3, help="Confidence threshold between 0 and 1")
+    parser.add_argument("--dest", type=str, default=None, help="Path to save result image")
+
+    args = parser.parse_args()
+
+    if not args.dest.endswith("/"):
+        args.dest += "/"
+
+    # Run main function
+    main(args.image, args.conf, args.dest)
